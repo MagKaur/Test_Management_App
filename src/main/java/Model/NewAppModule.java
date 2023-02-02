f13e5728cc3dba6830f4e9229ea4b65eadbc2c79
Magdalena Kochanowska <kochanowska.magd@gmail.com>
2023-02-02 23:24:20 +0100

    Changes: + TODO delated

--- src/main/java/Model/NewAppModule.java
+++ src/main/java/Model/NewAppModule.java
@@ -5,7 +5,7 @@ import com.sun.istack.NotNull;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "new_app_modules", schema = "mango") //TODO indeksy jednak w bazie danych tylko.
+@Table(name = "new_app_modules", schema = "mango")
 //TODO widok dla device models+ binaries stworzony w bazie
 public class NewAppModule {
     @Id
