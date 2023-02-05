42a186925708fec34db895e68a8db3b4ddcbd1dc
Magdalena Kochanowska <kochanowska.magd@gmail.com>
2023-02-05 15:02:08 +0100

    Changes: + few List<binary> findBy...in BinaryRepository

--- src/main/java/Model/BinaryRepository.java
+++ src/main/java/Model/BinaryRepository.java
@@ -2,14 +2,22 @@ package Model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
 
 public interface BinaryRepository extends JpaRepository<Binary, Integer> {
 
     @Override
     @RestResource(exported = false)
-    void delete (Binary binary);
+    void delete(Binary binary);
 
     @Override
     @RestResource(exported = false)
     void deleteById(Integer integer);
+
+    List<Binary> findByBinary_status(@NonNull String binary_status); //TODO czy to na pewno tak?
+    List<Binary> findByAccepted_statusIsTrue();
+    List<Binary> findByAccepted_statusIsFalse();
+    List<Binary> findByAccepted_statusIsTrueAndAccepted_statusIsFalse(); //TODO czy to tak się robi?
 }
