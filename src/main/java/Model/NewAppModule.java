717d5dc33784f227abc9f3ba04a894eabb3da91c
Magdalena Kochanowska <kochanowska.magd@gmail.com>
2023-02-08 17:29:07 +0100

    Changes: + Model.DeviceModel findBy In List correction

--- src/main/java/Model/NewAppModule.java
+++ src/main/java/Model/NewAppModule.java
@@ -4,7 +4,6 @@ package Model;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.*;
