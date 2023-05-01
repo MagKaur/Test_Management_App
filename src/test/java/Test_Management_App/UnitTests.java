267541a32a46785f3fd1460b2f2fbe1de3967532
Magdalena Kochanowska <kochanowska.magd@gmail.com>
2023-05-01 14:07:27 +0200

    + @SpringBootTest annotation added for UnitTests

--- src/test/java/Test_Management_App/UnitTests.java
+++ src/test/java/Test_Management_App/UnitTests.java
@@ -1,11 +1,12 @@
 package Test_Management_App;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class UnitTests {
 
     @Test
     public void NewAppModule() {
-
     }
 }
