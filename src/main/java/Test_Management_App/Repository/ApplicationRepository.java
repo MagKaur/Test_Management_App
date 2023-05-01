3c1ef4648da256cea2ca2f5a56fb1e838f9adf1d
Magdalena Kochanowska <kochanowska.magd@gmail.com>
2023-05-01 19:56:40 +0200

    + Application class added + ApplicationRepository class added

---
+++ src/main/java/Test_Management_App/Repository/ApplicationRepository.java
@@ -0,0 +1,10 @@
+package Test_Management_App.Repository;
+
+
+import Test_Management_App.Model.Application;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+interface ApplicationRepository extends JpaRepository<Application, Integer> {
+}
