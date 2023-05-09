abe40d6ddcbf1adf7502591fd06d2dee67175c35
Magdalena Kochanowska <kochanowska.magd@gmail.com>
2023-05-09 21:57:38 +0200

    + ApplicationController class with PatchMapping for updateApplication method
    to implement partial updates of Applications variables.

---
+++ src/main/java/Test_Management_App/Controller/ApplicationController.java
@@ -0,0 +1,27 @@
+package Test_Management_App.Controller;
+
+import Test_Management_App.Model.Application;
+import Test_Management_App.Payloads.ApplicationPartialUpdatePayload;
+import Test_Management_App.Service.ApplicationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/applications")
+public class ApplicationController {
+
+    private final ApplicationService applicationService;
+
+    public ApplicationController(ApplicationService applicationService) {
+        this.applicationService = applicationService;
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Application> updateApplication(
+            @PathVariable("id") int id_application,
+            @RequestBody ApplicationPartialUpdatePayload partialUpdate) {
+
+        Application updatedApplication = applicationService.partialUpdateApplication(id_application, partialUpdate);
+        return ResponseEntity.ok(updatedApplication);
+    }
+}
